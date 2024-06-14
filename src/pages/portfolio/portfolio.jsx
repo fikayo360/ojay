a170916e7d10f645af80ff1a287ba4cf58d70cdc
fikayo <fikayoadele@gmail.com>
2024-06-14 21:57:58 +0100

    final

---
+++ src/pages/portfolio/portfolio.jsx
@@ -0,0 +1,52 @@
+import './portfolio.css'
+
+const Portfolio = () => {
+    return (
+        <section id='portfolio'>
+            <h1>My works</h1>
+            <div id='portfolios'>
+                <div className='portfolio'>
+                    <img src="https://www.bellanaijaweddings.com/wp-content/uploads/2020/07/OcTillInfinity-WhiteWedding-BellaNaija-Weddings.066-1080x805.jpg" alt="" />
+                </div>
+                <div className='portfolio'>
+                    <img src="https://hips.hearstapps.com/edc.h-cdn.co/assets/17/01/1600x1066/wedding-photos-30.jpeg?resize=640:*" alt="" />
+                </div>
+                <div className='portfolio'>
+                    <img src="https://d32qys9a6wm9no.cloudfront.net/images/movies/backdrop/d9/434b2dd0d08812c97db07022b352fb6a_1280x720.jpg?t=1703070222" alt="" />
+                </div>
+                <div className='portfolio'>
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/fairy-and-j-1579625040.jpeg" alt="" />
+                </div>
+                <div className='portfolio'>
+                    <img src="https://wildflowerweddingphotography.com/wp-content/uploads/2019/10/17-7089-post/outdoor-hermann-wedding-01.jpg" alt="" />
+                </div>
+                <div className='portfolio'>
+                    <img src="https://www.cecilybreeding.com/uploads/9/9/4/2/9942134/katieanddoug-31_1_orig.jpg"  alt="" />
+                </div>
+                <div className='portfolio'>
+                    <img src="https://media-api.xogrp.com/images/f36e7a77-5bbe-46f5-8e88-38dd8229ebd3~rs_1024.445" alt="" />
+                </div>
+                <div className='portfolio'>
+                    <img src="https://richhowman.com/wp-content/uploads/2018/03/Huntsham-Court-Wedding-Photographer-768x513.jpg.webp" alt="" />
+                </div>
+                <div className='portfolio'>
+                    <img src="https://www.bellanaijaweddings.com/wp-content/uploads/2020/08/Daniella-Angelo-BellaNaija-Weddings.096.jpg" alt="" />
+                </div>
+                <div className='portfolio'>
+                    <img src="https://thewed.com/storage/photos/July2023/thumbnails/0.5x/wCqjPafridTQl8g8EXr6.jpg?1690111668" alt="" />
+                </div>
+                <div className='portfolio'>
+                    <img src="https://www.slanand.com/wp-content/uploads/2023/04/Wedding-Photo-SLA_012.webp" alt="" />
+                </div>
+                <div className='portfolio'>
+                    <img src="https://assets.easyweddings.com/files/2017/06/07101647/Most-popular-wedding-venues-Adelaide-SA-Serafino-McLaren-Vale-1.jpeg" alt="" />
+                </div>
+                <div className='portfolio'>
+                    <img src="https://i.pinimg.com/736x/25/2c/8d/252c8db6d32e5f7bc30bd78c213f0547.jpg" alt="" />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Portfolio
---
+++ src/pages/services/services.jsx
@@ -0,0 +1,17 @@
+import './services.css'
+
+const Services = () => {
+    return (<section id='services'>
+        <h1 id='sH'> My services </h1>
+        <div id='aServices'>
+            <div className='service' style={{ backgroundImage: `url(https://epiclovephotography.com/wp-content/uploads/2020/04/how-to-have-an-amazing-elopement-ceremony-northern-ireland-elopements_0002.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center' }}><h1>Elopements</h1></div>
+            <div className='service' style={{ backgroundImage: `url(https://chantaldavidson.com/wp-content/uploads/2020/05/Maggie-and-Alex-19-scaled.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center' }}><h1>Weddings</h1></div>
+            <div className='service' style={{ backgroundImage: `url(https://images.squarespace-cdn.com/content/v1/5bab088b65019fe4d5c9c73b/1693094880426-KK1K2PX5KO1SL1KPTFBF/Melbourne+celebrant+for+registry+office+wedding.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center' }}><h1>couples</h1></div>
+            <div className='service' style={{ backgroundImage: `url(https://shotkit.com/wp-content/uploads/2022/08/pregnancy-photo-ideas-garon-piceli.jpeg)`, backgroundSize: 'cover', backgroundPosition: 'center' }}><h1>maternity</h1></div>
+            <div className='service' style={{ backgroundImage: `url(https://www.telegraph.co.uk/content/dam/Travel/hotels/asia/indonesia/bali/como-shambhala-bali-villa2.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center' }}><h1>honeymoons</h1></div>
+        </div>
+
+    </section>)
+}
+
+export default Services
